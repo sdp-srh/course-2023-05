cec165380bf333bc34df1350cc2c717a793ce31a
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2023-05-25 18:06:13 +0200

    calculator and test servlets

--- ProjectWorkSample/src/main/java/application/CreatePerson.java
+++ ProjectWorkSample/src/main/java/application/CreatePerson.java
@@ -12,13 +12,13 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CreatePerson
  */
-public class CreatePersonServlet extends HttpServlet {
+public class CreatePerson extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreatePersonServlet() {
+    public CreatePerson() {
         super();
         // TODO Auto-generated constructor stub
     }
