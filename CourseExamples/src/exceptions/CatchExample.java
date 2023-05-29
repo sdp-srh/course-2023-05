df6ed8208fdba93cfbd2a11cb11bca51f7d72273
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2023-05-29 16:18:29 +0200

    crud and file io

---
+++ CourseExamples/src/exceptions/CatchExample.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+public class CatchExample {
+	
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 0;
+		int result = -1;
+				
+		
+		try {
+			result = a/b;
+		} catch (Exception e) {
+			System.out.println("Catch is only executed, if an exception has occured");
+			e.printStackTrace();
+		}
+		finally {
+			System.out.println("Finally is always executed");
+		}
+		System.out.println(result);
+	}
+}
--- CourseExamples/src/libraries/LibraryImportExample.java
+++ CourseExamples/src/libraries/LibraryImportExample.java
@@ -4,14 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.io.FileUtils;
+
 // import org.apache.commons.io.FileUtils;
 
 public class LibraryImportExample {
 
 	public static void main(String[] args) throws IOException {
 		File f = new File("C:\\srh\\example.txt");
-		String s = "Test";
-		// String s = FileUtils.readFileToString(f, StandardCharsets.UTF_8);
+		String s = FileUtils.readFileToString(f, StandardCharsets.UTF_8);
 		System.out.println(s);
 	}
 }
